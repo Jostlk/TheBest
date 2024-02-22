47a5074186082bcbf412bec33484014a10c4cfb1
Jostlk <evgenijlopyrev29@gmail.com>
2024-02-22 17:08:38 +0300

    FireBall

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float LifeTime;
+    public float Speed;
+    private void Start()
+    {
+        Invoke("DestroyFireBall", LifeTime);
+    }
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyFireBall();
+    }
+    void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * Speed * Time.fixedDeltaTime;
+    }
+    private void DestroyFireBall()
+    {
+        Destroy(gameObject);
+    }
+}
